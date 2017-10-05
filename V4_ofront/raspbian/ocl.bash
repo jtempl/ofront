54ec432b1a262147b2ecd2c53888f302d7cfe8ed
jtempl <josef.templ@gmail.com>
2017-10-05 16:56:57 +0200

    ofront version for Raspberry Pi running 32-bit Raspbian added

---
+++ V4_ofront/raspbian/ocl.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# compile an Oberon module <M>.Mod into a shared library lib<M>.so
+#
+# SYNOPSIS
+#   ocl.bash moduleName [ofrontOptions [ccOptions]]
+
+if [ -z "$1" ]; then
+  echo "Compile an Oberon module <M>.Mod into a shared library lib<M>.so."
+  echo
+  echo Usage: $0 moduleName [ofrontOptions [ccOptions]]
+  echo
+  echo Use single character - or "\"\"" to skip ofrontOptions.
+  echo Examples:
+  echo "  " $0 hello - -g
+  echo "  " $0 MyModule -s "\""-g -Lmypath -lMyImport1 -lMyImport2"\""
+  echo "  " $0 TestPi -s "\""-g -lWiringPi"\""
+else
+  # translate <M>.Mod to <M>.c
+  ofront $1.Mod $2
+  if [ -r $1.c ]; then
+    # compile and link <M>.c into lib<M>.so
+    rm -f lib$1.so
+    cc $1.c -shared -o lib$1.so -L. -lOberonV4 $3
+    # remove intermediate file(s) and show result
+    rm $1.c
+    ls -l lib$1.so
+  fi
+fi
