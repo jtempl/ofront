a2b0ce589142c9550890c31a5501e6fa31d16627
jtempl <josef.templ@gmail.com>
2017-10-19 14:17:02 +0200

    ocl.bash improved. Refs: #16. Can now also generate a main executable if ofornt option -m is specified. The generated .c file is removed before running ofornt, not at the end. The dependency on ofront being in the PATH has been removed.

--- V4_ofront/raspbian/ocl.bash
+++ V4_ofront/raspbian/ocl.bash
@@ -1,12 +1,14 @@
 #!/bin/bash
 #
 # compile an Oberon module <M>.Mod into a shared library lib<M>.so
+# or, if ofront option -m is specified, into an executable file <M>
 #
 # SYNOPSIS
 #   ocl.bash moduleName [ofrontOptions [ccOptions]]
 
 if [ -z "$1" ]; then
-  echo "Compile an Oberon module <M>.Mod into a shared library lib<M>.so."
+  echo "Compile an Oberon module <M>.Mod into a shared library lib<M>.so"
+  echo "or, if ofront option -m is specified, into an executable file <M>."
   echo
   echo Usage: $0 moduleName [ofrontOptions [ccOptions]]
   echo
@@ -17,13 +19,21 @@ if [ -z "$1" ]; then
   echo "  " $0 TestPi -s "\""-g -lWiringPi"\""
 else
   # translate <M>.Mod to <M>.c
-  ofront $1.Mod $2
+  rm -f $1.c
+  $OFRONT_HOME/bin/ofront $1.Mod $2
   if [ -r $1.c ]; then
-    # compile and link <M>.c into lib<M>.so
-    rm -f lib$1.so
-    cc $1.c -I$OFRONT_HOME/V4_ofront -shared -o lib$1.so -L. -L$OFRONT_HOME/lib -lOberonV4 $3
-    # remove intermediate file(s) and show result
-    rm $1.c
-    ls -l lib$1.so
+    if [[ $2 =~ .*m.* ]]; then
+      # compile and link <M>.c into executable <M>
+      rm -f $1
+      cc $1.c -I$OFRONT_HOME/V4_ofront -o $1 -L. -L$OFRONT_HOME/lib -lOberonV4 $3
+      # show result
+      ls -l $1
+    else
+      # compile and link <M>.c into lib<M>.so
+      rm -f lib$1.so
+      cc $1.c -I$OFRONT_HOME/V4_ofront -shared -o lib$1.so -L. -L$OFRONT_HOME/lib -lOberonV4 $3
+      # show result
+      ls -l lib$1.so
+    fi
   fi
 fi
